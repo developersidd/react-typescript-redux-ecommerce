5f5d24741bb265cd1d00495f37860ee77595fea4
AB Siddik <siddik.prgmr@gmail.com>
2023-09-12 15:42:53 +0600

    changes

--- src/components/CategoriesItem/CategoriesItem.tsx
+++ src/components/CategoriesItem/CategoriesItem.tsx
@@ -3,7 +3,7 @@ import './CategoriesItem.css';
 
 const CategoriesItem = ({ name, img }: { name: string, img: string }) => {
     return (
-        <div className="relative z-20 h-96 bg-no-repeat flex flex-col justify-center items-center bg-cover lg:bg-[length:640px_640px]" style={{ backgroundImage: `url(${img})` }}>
+        <div className="relative z-20 h-96 bg-no-repeat flex flex-col justify-center items-center bg-cover bg-top " style={{ backgroundImage: `url(${img})` }}>
             <div className='absolute top-0 left-0 w-full h-full bg-black/40 z-[-1]' />
             <h3 className="mb-3 text-2xl text-white font-semibold">{name} </h3>
             <NavLink to="/shop"
